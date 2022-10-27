08cf8a6911c1cf5b8485d713ac355442a1054dc7
NAAAAAAAAA1212 <96600575+NAAAAAAAAA1212@users.noreply.github.com>
2022-10-27 09:48:43 +0800

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-let mySprite = sprites.create(img`
+info.onLifeZero(function () {
+    info.setLife(10000)
+    tiles.placeOnRandomTile(mySprite, assets.tile`我的貼圖2`)
+})
+let mySprite: Sprite = null
+info.setLife(10000)
+mySprite = sprites.create(img`
     . . . . . d d d d d d . . . . . 
     . . . . d . . . . . . d . . . . 
     . . . . d . d . . d . d . . . . 
@@ -21,3 +27,23 @@ scene.cameraFollowSprite(mySprite)
 scene.setBackgroundColor(15)
 tiles.setCurrentTilemap(tilemap`層級1`)
 tiles.placeOnRandomTile(mySprite, assets.tile`我的貼圖2`)
+forever(function () {
+    if (mySprite.tileKindAt(TileDirection.Center, assets.tile`我的貼圖15`)) {
+        info.changeLifeBy(-1)
+    } else {
+        if (info.life() != 10000) {
+            info.changeLifeBy(1)
+        }
+    }
+    pause(100)
+})
+forever(function () {
+    if (mySprite.tileKindAt(TileDirection.Center, sprites.dungeon.floorLight0)) {
+        info.setLife(10000)
+    }
+})
+forever(function () {
+    if (mySprite.tileKindAt(TileDirection.Center, assets.tile`我的貼圖16`)) {
+        info.setLife(10000)
+    }
+})
