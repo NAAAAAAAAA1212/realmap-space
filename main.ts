68ce5c1c680a578d2f4688f11a2851d9715222da
NAAAAAAAAA1212 <96600575+NAAAAAAAAA1212@users.noreply.github.com>
2022-12-09 18:09:17 +0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,18 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖27`, function (sprite, location) {
-    if (admin_lock == 0) {
-        admin_lock = 1
-        tiles.setTileAt(tiles.getTileLocation(5, 29), assets.tile`我的貼圖35`)
-        tiles.setTileAt(tiles.getTileLocation(5, 31), assets.tile`我的貼圖35`)
-        tiles.setTileAt(tiles.getTileLocation(5, 33), assets.tile`我的貼圖35`)
-        tiles.setTileAt(tiles.getTileLocation(5, 35), assets.tile`我的貼圖35`)
-        tiles.setTileAt(tiles.getTileLocation(5, 37), assets.tile`我的貼圖35`)
-        tiles.setTileAt(tiles.getTileLocation(5, 39), assets.tile`我的貼圖35`)
-        tiles.setTileAt(tiles.getTileLocation(5, 41), assets.tile`我的貼圖35`)
-        tiles.setWallAt(tiles.getTileLocation(5, 29), true)
-        tiles.setWallAt(tiles.getTileLocation(5, 31), true)
-        tiles.setWallAt(tiles.getTileLocation(5, 33), true)
-        tiles.setWallAt(tiles.getTileLocation(5, 35), true)
-        tiles.setWallAt(tiles.getTileLocation(5, 37), true)
-        tiles.setWallAt(tiles.getTileLocation(5, 39), true)
-        tiles.setWallAt(tiles.getTileLocation(5, 41), true)
-        pause(10000)
-        admin_lock = 0
-        tiles.setTileAt(tiles.getTileLocation(5, 29), assets.tile`我的貼圖32`)
-        tiles.setTileAt(tiles.getTileLocation(5, 31), assets.tile`我的貼圖32`)
-        tiles.setTileAt(tiles.getTileLocation(5, 33), assets.tile`我的貼圖32`)
-        tiles.setTileAt(tiles.getTileLocation(5, 35), assets.tile`我的貼圖32`)
-        tiles.setTileAt(tiles.getTileLocation(5, 37), assets.tile`我的貼圖32`)
-        tiles.setTileAt(tiles.getTileLocation(5, 39), assets.tile`我的貼圖32`)
-        tiles.setTileAt(tiles.getTileLocation(5, 41), assets.tile`我的貼圖32`)
-        tiles.setWallAt(tiles.getTileLocation(5, 29), false)
-        tiles.setWallAt(tiles.getTileLocation(5, 31), false)
-        tiles.setWallAt(tiles.getTileLocation(5, 33), false)
-        tiles.setWallAt(tiles.getTileLocation(5, 35), false)
-        tiles.setWallAt(tiles.getTileLocation(5, 37), false)
-        tiles.setWallAt(tiles.getTileLocation(5, 39), false)
-        tiles.setWallAt(tiles.getTileLocation(5, 41), false)
-    }
+    tiles.setTileAt(tiles.getTileLocation(5, 29), assets.tile`我的貼圖35`)
+    tiles.setTileAt(tiles.getTileLocation(5, 31), assets.tile`我的貼圖35`)
+    tiles.setTileAt(tiles.getTileLocation(5, 33), assets.tile`我的貼圖35`)
+    tiles.setTileAt(tiles.getTileLocation(5, 35), assets.tile`我的貼圖35`)
+    tiles.setTileAt(tiles.getTileLocation(5, 37), assets.tile`我的貼圖35`)
+    tiles.setTileAt(tiles.getTileLocation(5, 39), assets.tile`我的貼圖35`)
+    tiles.setTileAt(tiles.getTileLocation(5, 41), assets.tile`我的貼圖35`)
+    tiles.setWallAt(tiles.getTileLocation(5, 29), true)
+    tiles.setWallAt(tiles.getTileLocation(5, 31), true)
+    tiles.setWallAt(tiles.getTileLocation(5, 33), true)
+    tiles.setWallAt(tiles.getTileLocation(5, 35), true)
+    tiles.setWallAt(tiles.getTileLocation(5, 37), true)
+    tiles.setWallAt(tiles.getTileLocation(5, 39), true)
+    tiles.setWallAt(tiles.getTileLocation(5, 41), true)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖20`, function (sprite, location) {
     emergency_meeting = randint(1, 4)
@@ -51,6 +32,22 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖20`, function (sp
     pause(500)
     game.splash("EMERGENCY MEETING")
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖36`, function (sprite, location) {
+    tiles.setTileAt(tiles.getTileLocation(5, 29), assets.tile`我的貼圖32`)
+    tiles.setTileAt(tiles.getTileLocation(5, 31), assets.tile`我的貼圖32`)
+    tiles.setTileAt(tiles.getTileLocation(5, 33), assets.tile`我的貼圖32`)
+    tiles.setTileAt(tiles.getTileLocation(5, 35), assets.tile`我的貼圖32`)
+    tiles.setTileAt(tiles.getTileLocation(5, 37), assets.tile`我的貼圖32`)
+    tiles.setTileAt(tiles.getTileLocation(5, 39), assets.tile`我的貼圖32`)
+    tiles.setTileAt(tiles.getTileLocation(5, 41), assets.tile`我的貼圖32`)
+    tiles.setWallAt(tiles.getTileLocation(5, 29), false)
+    tiles.setWallAt(tiles.getTileLocation(5, 31), false)
+    tiles.setWallAt(tiles.getTileLocation(5, 33), false)
+    tiles.setWallAt(tiles.getTileLocation(5, 35), false)
+    tiles.setWallAt(tiles.getTileLocation(5, 37), false)
+    tiles.setWallAt(tiles.getTileLocation(5, 39), false)
+    tiles.setWallAt(tiles.getTileLocation(5, 41), false)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖17`, function (sprite, location) {
     tiles.setCurrentTilemap(blackhole)
     tiles.placeOnRandomTile(mySprite, assets.tile`我的貼圖2`)
@@ -68,8 +65,6 @@ let emergency_meeting = 0
 let mySprite: Sprite = null
 let blackhole: tiles.TileMapData = null
 let normal: tiles.TileMapData = null
-let admin_lock = 0
-admin_lock = 0
 info.setLife(1800)
 normal = tilemap`層級1`
 blackhole = tilemap`層級9`
